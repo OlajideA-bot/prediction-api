fe8cdf6d401ca7319171112d5ac151740d222d05
OfficialAvage <olajideazeez69@gmail.com>
2026-06-26 06:01:43 +0100

    Enable CORS for frontend access

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const predictionRouter = require('./routes/prediction');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
